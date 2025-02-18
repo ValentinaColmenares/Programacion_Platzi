1ad8a7e615dd2d40c621b934c98a0e8792342e40
Valentina <vale_colmenares@hotmail.com>
2025-02-18 09:59:21 -0500

    Clase 19 Ubicación de etiqueta script y reconocimiento de  selección de mascota

--- Mokepon/js/mokepon.js
+++ Mokepon/js/mokepon.js
@@ -1,6 +1,18 @@
-function seleccionarMascotaJugador(){
-    alert('SELECCIONASTE TU MASCOTA')
+function iniciarJuego(){
+    let botonMascotaJugador = document.getElementById('boton-mascota')
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
 }
 
-let botonMascotaJugador = document.getElementById('boton-mascota')
-botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+function seleccionarMascotaJugador(){
+    if (document.getElementById('hipodoge').checked){
+        alert('SELECCIONASTE HIPODOGE')
+    }else if(document.getElementById('capipepo').checked){
+        alert('SELECCIONASTE CAPIPEPO')
+    }else if(document.getElementById('ratigueya').checked){
+        alert('SELECCIONASTE RATIGUEYA')
+    }else{
+        alert('NO HAS SELECCIONADO TU MASCOTA')
+    }
+}
+
+window.addEventListener('load', iniciarJuego)
